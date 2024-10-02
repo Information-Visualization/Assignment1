e41bb12eccbf52f6d24bb6a5ddc5617aac5a2fd7
Natalie Degraffinried <nd2657@nyu.edu>
2024-10-02 04:52:31 +0000

    successfully uploaded and split list by city

--- assignment_1_code.py
+++ assignment_1_code.py
@@ -1,8 +1,36 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
-file_path = "./data/climate.csv"
+import csv #used Python documentation to work this out
+#import numpy as np
 
 
-for item in 
+file_path = "./data/climate.csv" 
+climate_info = []
+with open(file_path) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        climate_info.append(row)
+
+chicago_data = []
+houston_data = []
+miami_data = []
+ny_data = []
+sf_data = []
+seattle_data = []
+
+for item in climate_info:
+    if item["city"] == "Chicago":
+        chicago_data.append(item)
+    elif item["city"] == "Houston":
+        houston_data.append(item)
+    elif item["city"] == "Miami":
+        miami_data.append(item)
+    elif item["city"] == "New York":
+        ny_data.append(item)
+    elif item["city"] == "San Francisco":
+        sf_data.append(item)
+    elif item["city"] == "Seattle":
+        seattle_data.append(item)
+
+#used something similar in another class
+def distance(city1, city2):
+    
---
+++ scratchpad.py
@@ -0,0 +1,26 @@
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
+
+#file_path = "./data/climate.csv"
+#climate_info = DataFrame.to_dict(pd.read_csv(file_path))
+
+#for city in climate_info:
+#    if city
+#        print(climate_info["lat"], climate_info["lon"])
+
+
+#for cities in climate_info:
+#    if climate_info.item("city") == "Chicago":
+#        print(climate_info["lat"], climate_info["lon"])
+
+#for city in climate_info:
+#    if "city" == "Chicago":
+#        print("lat", "lon")
+
+#print(chicago_data[0])
+#print(houston_data[0])
+#print(miami_data[0])
+#print(ny_data[0])
+#print(sf_data[0])
+#print(seattle_data[0])
